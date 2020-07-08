11d1c3ed5ea2322e6eb5242e0c769da86fb602b7
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-07-08 09:45:44 +0300

    Updates

---
+++ precache-manifest.05268f6f545f640fb560e56298e90507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a962de8473349d048252e1784d3a645",
+    "url": "/usewindowsize/index.html"
+  },
+  {
+    "revision": "c4a510a0ddfd90b639a0",
+    "url": "/usewindowsize/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b28b87bb76bda7186d97",
+    "url": "/usewindowsize/static/js/2.25c5d37d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/usewindowsize/static/js/2.25c5d37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a510a0ddfd90b639a0",
+    "url": "/usewindowsize/static/js/main.f387e769.chunk.js"
+  },
+  {
+    "revision": "8fcd5a8b491acad26edc",
+    "url": "/usewindowsize/static/js/runtime-main.b4cd4c9a.js"
+  }
+]);
