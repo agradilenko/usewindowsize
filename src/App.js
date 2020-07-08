7a7daacb1f11dd52d0ab3e99e11e6f98825884d9
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-07-08 09:44:25 +0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 const { useEffect, useState } = React;
 
 function useWindowSize() {
@@ -7,9 +7,8 @@ function useWindowSize() {
   useEffect(() => {
     const handleResize = () => {
       setSize([window.innerHeight, window.innerWidth]);
-    }
+    };
     window.addEventListener("resize", handleResize);
-    // Clean up!
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -20,11 +19,16 @@ function useWindowSize() {
 function App() {
   const [height, width] = useWindowSize();
   return (
-    <div className="App" style={{backgroundColor: "red", width: width , height: height }}>
-       <p style={{margin: 0}}>
-           height: {height}<br />
-           width: {width}<br />
-        </p>
+    <div
+      className="App"
+      style={{ backgroundColor: "red", width: width, height: height }}
+    >
+      <p style={{ margin: 0 }}>
+        height: {height}
+        <br />
+        width: {width}
+        <br />
+      </p>
     </div>
   );
 }
