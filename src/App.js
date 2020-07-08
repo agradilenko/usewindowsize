28a8d93b34445ca3909e78ab87037a57b5fd9205
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-07-08 09:33:40 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+const { useEffect, useState } = React;
+
+function useWindowSize() {
+  const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize([window.innerHeight, window.innerWidth]);
+    }
+    window.addEventListener("resize", handleResize);
+    // Clean up!
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return size;
+}
+
+function App() {
+  const [height, width] = useWindowSize();
+  return (
+    <div className="App" style={{backgroundColor: "red", width: width , height: height }}>
+       <p style={{margin: 0}}>
+           height: {height}<br />
+           width: {width}<br />
+        </p>
+    </div>
+  );
+}
+
+export default App;
